80825e4e9ce670791c226f50686c54b6ab6fe81d
FoxerLee <foxerlee1@gmail.com>
2018-02-05 22:29:42 +0800

    fix bug

--- src/main/java/com/monetware/demo/variable/root.java
+++ src/main/java/com/monetware/demo/variable/root.java
@@ -4,6 +4,6 @@ package com.monetware.demo.variable;
  * Created by Stephen on 2017/12/4.
  */
 public class root {
-    public static String FileRoot = "/Users/liyuan/Documents/软件工程/demo/src/main/resources/static/files/";
+    public static String FileRoot = "/home/twoRaven/monet/";
 
 }
